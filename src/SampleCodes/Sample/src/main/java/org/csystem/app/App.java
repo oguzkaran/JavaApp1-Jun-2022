2630e50637fc6a605ca41c0c179a4ca16dd08f7c
Oğuz Karan <oguzkaran@csystem.org>
2022-07-23 14:38:33 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
@@ -1,33 +1,28 @@
 /*----------------------------------------------------------------------------------------------------------------------
-   Sınıf Çalışması: Parametresi ile aldığı int türden bir sayının 1(bir) olan bitlerinin indeks numaralarını int türden bir dizi
-   olarak döndüren indicesOfSetBits metodunu BitwiseUtil sınıfı içerisinde yazınız ve aşağıdaki kod ile test ediniz
+    Calendar sınıfı (dolayısıyla GregorianCalendar sınıfı) tarih-zaman değerleri için geçerlilik kontrolü yapmaz. Geçerli
+    olmayan değerleri hesaplayarak (ekleyerek veya çıkartarak) uygun ntarih zamana dönüştürür
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-import org.csystem.util.array.ArrayUtil;
-import org.csystem.util.bitwise.BitwiseUtil;
 import org.csystem.util.console.Console;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 class App {
     public static void main(String[] args)
     {
-        IndicesOfSetBitsTest.run();
+        var now = new GregorianCalendar(2021, Calendar.SEPTEMBER, 123456, 89, 1);
+
+        Console.writeLine("%02d/%02d/%04d %02d:%02d:%02d.%02d",
+                now.get(Calendar.DAY_OF_MONTH),
+                now.get(Calendar.MONTH) + 1,
+                now.get(Calendar.YEAR),
+                now.get(Calendar.HOUR_OF_DAY),
+                now.get(Calendar.MINUTE),
+                now.get(Calendar.SECOND),
+                now.get(Calendar.MILLISECOND));
+
+
     }
 }
-
-class IndicesOfSetBitsTest {
-    public static void run()
-    {
-        for (;;) {
-            var a = Console.readShort("Bir sayı giriniz:");
-
-            Console.writeLine(BitwiseUtil.toBitsStr(a));
-
-            ArrayUtil.print(BitwiseUtil.indicesOfSetBits(a));
-
-            if (a == 0)
-                break;
-        }
-    }
-}
-
