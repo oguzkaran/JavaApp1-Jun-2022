fa956368f962a1bb15d80d24174e6d645231d638
Oğuz Karan <oguzkaran@csystem.org>
2022-07-23 14:45:50 +0300

    Update

---
+++ src/Libraries/JavaLibs/DateTimeLibLegacyLib/src/main/java/org/csystem/util/datetime/DateTimeException.java
@@ -0,0 +1,10 @@
+package org.csystem.util.datetime;
+public class DateTimeException extends RuntimeException  {
+    public DateTimeException()
+    {}
+
+    public DateTimeException(String msg)
+    {
+        super(msg);
+    }
+}
---
+++ src/Libraries/JavaLibs/DateTimeLibLegacyLib/src/main/java/org/csystem/util/datetime/Month.java
@@ -0,0 +1,4 @@
+package org.csystem.util.datetime;
+public enum Month {
+    JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
+}
