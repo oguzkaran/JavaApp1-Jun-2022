0b6d48f0bbcf0d3f3135683292a867284021566a
Oğuz Karan <oguzkaran@csystem.org>
2022-07-31 14:44:35 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
@@ -1,35 +1,106 @@
 /*----------------------------------------------------------------------------------------------------------------------
-   assert statement:
+    Aşağıdaki örnekte enlargaCapacity metodu private olduğundan normal şartlarda public bir metot gibi argüman kontrolü
+    yapılmaz. Çünkü zaten private metot programcı tarafından uygun argümanlarla çağrılır. Fakat bu durumunda programcı
+    sınıfı yazarken hata yapma olasılığına karşılık enlargeCapacity metodunun argüman değerlerini kontrol edebilir. Hatta
+    böylesi durumlar kritikse kontrol etmelidir. İşte bu test basit olarak assert deyimi ile yapılabilir. İleride göreceğimizi
+    söylediğimiz assert deyimlerinin nihai üründen kaldırılması durumu assert deyimlerinin ürünü size olarak büyütmesini
+    engelleyecektir. Aşağıdaki kodda -a kaldırılsa da assert deyimleri orada kalır ancak görmezden gelinir. add metodunda
+    * ile belirtilen çağrıyı enlargeCapacity(m_elems.length * 2); biçiminde değiştirerek ve trimToSize metosunda ** ile
+    belirtilen if deyimini kaldırarak, JVM'i -ea ile çalışıtırp sonucu gözlemleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 import org.csystem.util.console.Console;
-import org.csystem.util.datetime.stopwatch.StopWatch;
-import org.csystem.util.numeric.NumberUtil;
 
-import java.math.BigInteger;
-import java.util.Random;
+import java.util.Arrays;
 
 class App {
     public static void main(String[] args)
     {
-        IsPrimeTest.run();
+        var list = new IntList(5);
+
+        Console.writeLine("Capacity:%d", list.capacity());
+        Console.writeLine("Size:%d", list.size());
+
+        for (int i = 0; i < 11; ++i)
+            list.add(i * 10);
+
+        Console.writeLine("Capacity:%d", list.capacity());
+        Console.writeLine("Size:%d", list.size());
+
+        list.trimToSize();
+
+        Console.writeLine("Capacity:%d", list.capacity());
+        Console.writeLine("Size:%d", list.size());
+
+        list.clear();
+
+        Console.writeLine("Capacity:%d", list.capacity());
+        Console.writeLine("Size:%d", list.size());
+
+        list.trimToSize();
+
+        Console.writeLine("Capacity:%d", list.capacity());
+        Console.writeLine("Size:%d", list.size());
+
+        list.add(10);
     }
 }
 
-class IsPrimeTest {
-    public static void run()
+class IntList {
+    private static final int DEFAULT_CAPACITY = 10;
+    private int [] m_elems;
+    private int m_index;
+
+    private void enlargeCapacity(int newCapacity)
     {
-        var random = new Random();
-        var stopWatch = new StopWatch();
-        var nBits = Console.readInt("Kaç bitlik bir asal sayı üretilsin?");
-        var val = BigInteger.probablePrime(nBits, random);
+        assert newCapacity != 0 : "newCapacity can not be zero";
+        assert newCapacity >= m_index : newCapacity + ", " + m_index;
+        m_elems = Arrays.copyOf(m_elems, newCapacity);
+    }
 
-        Console.writeLine("val = %s", val);
+    public IntList()
+    {
+        m_elems = new int[DEFAULT_CAPACITY];
+    }
 
-        stopWatch.start();
-        Console.writeLine(NumberUtil.isPrime(val) ? "Asal" : "Asal değil");
-        stopWatch.stop();
-        Console.writeLine("Elapsed (sec):%f", stopWatch.totalSeconds());
+    public IntList(int initialCapacity)
+    {
+        m_elems = new int[initialCapacity];
+    }
+
+    public void add(int value)
+    {
+        if (m_elems.length == m_index)
+            enlargeCapacity(m_elems.length == 0 ? 1 : m_elems.length * 2); //*
+
+        m_elems[m_index++] = value;
+    }
+
+    public int get(int index)
+    {
+        //...
+        return m_elems[index];
+    }
+
+    public void clear()
+    {
+        m_index = 0;
+    }
+
+    public int capacity()
+    {
+        return m_elems.length;
+    }
+
+    public int size()
+    {
+        return m_index;
+    }
+
+    public void trimToSize()
+    {
+        if (m_index != 0) //**
+            enlargeCapacity(m_index);
     }
 }
