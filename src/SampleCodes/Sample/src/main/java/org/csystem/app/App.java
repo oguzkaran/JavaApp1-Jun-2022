1529b875fc2ab395997a09ea009c4a43c064eb76
Oğuz Karan <oguzkaran@csystem.org>
2022-08-06 14:28:38 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
@@ -1,51 +1,17 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    static sınıflar static elemanlar olduklarından "import static single type declaration" bildirimi ilem niteliksiz
-    (unqualified) olarak kullanılabilir. Ancak bu durumda "top-level" sınıf gibi eriişildiğinden okunabilirliğin
-    bozulmamasına dikkat edilmelidir
+    Bir sınıfın içerisinde non-static bir sınıf bvildirilebilir (inner class)
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
-
-import org.csystem.util.console.Console;
-import static org.csystem.app.A.B;
 
 class App {
     public static void main(String[] args)
     {
-        var b = new B();
-        //...
+
     }
 }
 
 class A {
-    public static class B {
-        private void foo()
-        {
-            Console.writeLine("B.foo");
-        }
-
-        private int m_x;
-
-        public int getX()
-        {
-            return m_x;
-        }
-
-        public void setX(int x)
-        {
-            m_x = x;
-        }
-
-        public void bar()
-        {
-            Console.writeLine("B.bar");
-        }
-
-        public static void tar()
-        {
-            Console.writeLine("B.tar");
-        }
+    public class B {
+        //...
     }
-
-
 }
-
