a0d15a4e34db4295ced803880c3a14277c8c0c58
Oğuz Karan <oguzkaran@csystem.org>
2022-08-07 14:35:38 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
@@ -1,33 +1,52 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Yerel sınıflar (local classes): Bir metot içerisinde bildirilen sınıflara denir. Farklı faaliyet alanları (scope)
-    içerisinde aynı isimde yerel sınıf bildirimi geçerlidir. Aynı faaliyet alanı içerisinde aynı isimde sınıf bildirimi
-    geçersizdir
+    Aşağıdaki örneği inceleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import org.csystem.util.console.Console;
 
 class App {
     public static void main(String[] args)
     {
+        var iv = Util.createIntValueByStdin();
+        var a = Console.readInt("Üçüncü sayıyı giriniz:");
 
+        Util.doWork(a, iv);
     }
 }
 
-class A {
+class Util {
+    public static IntValue createIntValueByStdin()
+    {
+        var x = Console.readInt("Birinci sayıyı giriniz:");
+        var y = Console.readInt("İkinci sayıyı giriniz:");
+
+        return new IntValue(y) {
+            public int addWith(int value)
+            {
+                return x + value + a;
+            }
+        };
+    }
+
+    public static void doWork(int value, IntValue intValue)
+    {
+        Console.writeLine(intValue.addWith(value));
+    }
+}
+
+abstract class IntValue {
+    protected int a;
+    public IntValue()
+    {
+        //...
+    }
+
+    public IntValue(int a)
+    {
+        this.a = a;
+    }
+
+    public abstract int addWith(int value);
     //...
-}
-
-class Sample {
-    public  void foo()
-    {
-        class A {
-            //...
-        }
-    }
-
-    public  void bar()
-    {
-        class A {
-            //...
-        }
-    }
 }
