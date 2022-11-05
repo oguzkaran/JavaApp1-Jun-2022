b794e419a21b8feba9a9761f2b398a76dc91380a
Oğuz Karan <oguzkaran@csystem.org>
2022-11-05 16:58:20 +0300

    Update

---
+++ src/Libraries/JavaLibs/CollectionLib/src/main/java/org/csystem/util/collection/CollectionUtil.java
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------
+	FILE		: CollectionUtil.java
+	AUTHOR		: JavaApp1-Jun-2022 Group
+	LAST UPDATE	: 05.11.2022
+
+	Utility class for generic collection operations
+
+	Copyleft (c) 1993 C and System Programmers Association
+	All Rights Free
+----------------------------------------------------------------*/
+package org.csystem.util.collection;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public final class CollectionUtil {
+    private CollectionUtil()
+    {
+    }
+
+    public static <T, R> List<R> toList(Iterable<? extends T> iterable, Function<T, R> function)
+    {
+        return toList(iterable, function, false);
+    }
+
+    public static <T, R> List<R> toList(Iterable<? extends T> iterable, Function<T, R> function, boolean parallel)
+    {
+        return StreamSupport.stream(iterable.spliterator(), parallel).map(function).collect(Collectors.toList());
+    }
+
+    public static <T, R> Iterable<R> toIterable(Iterable<? extends T> iterable, Function<T, R> function)
+    {
+        return toIterable(iterable, function, false);
+    }
+
+    public static <T, R> Iterable<R> toIterable(Iterable<? extends T> iterable, Function<T, R> function, boolean parallel)
+    {
+        return toList(iterable, function, parallel);
+    }
+}
