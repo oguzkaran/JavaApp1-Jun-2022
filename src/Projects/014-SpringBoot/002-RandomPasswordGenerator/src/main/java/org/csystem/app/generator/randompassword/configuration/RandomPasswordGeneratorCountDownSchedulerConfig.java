4e8615fd16ab4fca6a0fafecd5f1e25f474fbc27
Oğuz Karan <oguzkaran@csystem.org>
2022-11-26 14:47:31 +0300

    Update

--- src/Projects/014-SpringBoot/002-RandomPasswordGenerator/src/main/java/org/csystem/app/generator/randompassword/configuration/RandomPasswordGeneratorCountDownSchedulerConfig.java
+++ src/Projects/014-SpringBoot/002-RandomPasswordGenerator/src/main/java/org/csystem/app/generator/randompassword/configuration/RandomPasswordGeneratorCountDownSchedulerConfig.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
--- src/Projects/014-SpringBoot/002-RandomPasswordGenerator/src/main/java/org/csystem/app/generator/randompassword/configuration/runner/RandomNumberGeneratorRunner.java
+++ src/Projects/014-SpringBoot/002-RandomPasswordGenerator/src/main/java/org/csystem/app/generator/randompassword/configuration/runner/RandomNumberGeneratorRunner.java
@@ -1,8 +1,8 @@
 package org.csystem.app.generator.randompassword.configuration.runner;
 
+import org.csystem.util.console.Console;
 import org.csystem.util.scheduler.countdown.CountDownScheduler;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +17,7 @@ public class RandomNumberGeneratorRunner {
     }
 
     @Bean
-    public ApplicationRunner run(@Value("${app.random.count}")int count, @Value("${app.scheduler.interval}")long interval)
+    public ApplicationRunner run()
     {
         return args -> m_countDownScheduler.start();
     }
