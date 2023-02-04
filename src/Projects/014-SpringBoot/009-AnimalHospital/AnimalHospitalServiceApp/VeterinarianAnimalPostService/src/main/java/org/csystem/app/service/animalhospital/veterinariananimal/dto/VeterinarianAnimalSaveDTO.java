4698ecbb863f9e5ee5d9dd45919440ca100a81c6
Oğuz Karan <oguzkaran@csystem.org>
2023-02-04 13:57:40 +0300

    Update

--- src/Projects/014-SpringBoot/009-AnimalHospital/AnimalHospitalServiceApp/VeterinarianAnimalPostService/src/main/java/org/csystem/app/service/animalhospital/veterinariananimal/dto/VeterinarianAnimalSaveDTO.java
+++
@@ -1,7 +0,0 @@
-package org.csystem.app.service.animalhospital.veterinariananimal.dto;
-
-public class VeterinarianAnimalSaveDTO {
-    public int animalId;
-    public long diplomaNo;
-    public double price;
-}
