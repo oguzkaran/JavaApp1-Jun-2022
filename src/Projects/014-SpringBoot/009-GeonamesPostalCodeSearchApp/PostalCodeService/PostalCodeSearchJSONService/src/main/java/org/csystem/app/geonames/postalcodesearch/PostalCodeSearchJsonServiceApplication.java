c3d46d601ef11e07e21b7a8d0e94e7c52612945a
Oğuz Karan <oguzkaran@csystem.org>
2023-02-04 14:31:41 +0300

    Update

---
+++ src/Projects/014-SpringBoot/009-GeonamesPostalCodeSearchApp/PostalCodeService/PostalCodeSearchJSONService/src/main/java/org/csystem/app/geonames/postalcodesearch/PostalCodeSearchJsonServiceApplication.java
@@ -0,0 +1,14 @@
+package org.csystem.app.geonames.postalcodesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalCodeSearchJsonServiceApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PostalCodeSearchJsonServiceApplication.class, args);
+    }
+
+}
---
+++ src/Projects/014-SpringBoot/009-GeonamesPostalCodeSearchApp/PostalCodeService/PostalCodeSearchJSONService/src/test/java/org/csystem/app/geonames/postalcodesearch/PostalCodeSearchJsonServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package org.csystem.app.geonames.postalcodesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostalCodeSearchJsonServiceApplicationTests {
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
