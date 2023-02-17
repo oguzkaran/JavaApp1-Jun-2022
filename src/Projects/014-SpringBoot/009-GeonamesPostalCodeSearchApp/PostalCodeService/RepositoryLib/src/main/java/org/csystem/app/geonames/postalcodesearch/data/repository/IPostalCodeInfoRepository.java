66277af6a73f9a2280e28c9124ce370812133f30
Oğuz Karan <oguzkaran@csystem.org>
2023-02-17 13:45:38 +0300

    Update IPostalCodeInfoRepository.java

--- src/Projects/014-SpringBoot/009-GeonamesPostalCodeSearchApp/PostalCodeService/RepositoryLib/src/main/java/org/csystem/app/geonames/postalcodesearch/data/repository/IPostalCodeInfoRepository.java
+++ src/Projects/014-SpringBoot/009-GeonamesPostalCodeSearchApp/PostalCodeService/RepositoryLib/src/main/java/org/csystem/app/geonames/postalcodesearch/data/repository/IPostalCodeInfoRepository.java
@@ -1,7 +1,10 @@
 package org.csystem.app.geonames.postalcodesearch.data.repository;
 
 import org.csystem.app.geonames.postalcodesearch.data.entity.PostalCodeInfo;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
