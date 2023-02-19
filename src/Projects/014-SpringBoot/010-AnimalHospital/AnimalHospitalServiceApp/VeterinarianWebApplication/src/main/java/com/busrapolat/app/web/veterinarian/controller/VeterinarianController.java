4c5793e201118f7d19f4ff2d3a6b99e1cc29ed3c
Oğuz Karan <oguzkaran@csystem.org>
2023-02-19 14:59:10 +0300

    Update

--- src/Projects/014-SpringBoot/010-AnimalHospital/AnimalHospitalServiceApp/VeterinarianWebApplication/src/main/java/com/busrapolat/app/web/veterinarian/controller/VeterinarianController.java
+++ src/Projects/014-SpringBoot/010-AnimalHospital/AnimalHospitalServiceApp/VeterinarianWebApplication/src/main/java/com/busrapolat/app/web/veterinarian/controller/VeterinarianController.java
@@ -1,23 +1,43 @@
 package com.busrapolat.app.web.veterinarian.controller;
 
+import com.busrapolat.app.web.veterinarian.model.VeterinarianModel;
+import com.busrapolat.app.web.veterinarian.model.VeterinarianSaveModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/veterinarians")
 public class VeterinarianController {
-    @GetMapping("veterinarian/add")
-    public String index(Model model)
+    @GetMapping("/veterinarian")
+    public String veterinarianForm(Model model)
     {
+        model.addAttribute("veterinarian", new VeterinarianSaveModel());
+
         return "add";
     }
 
-    @PostMapping("addveterinarian")
-    public String addVeterinarian()
+    @PostMapping("/addveterinarian")
+    public String veterinarianSubmit(Model model, @ModelAttribute VeterinarianSaveModel veterinarian)
     {
-        throw new UnsupportedOperationException("TODO:");
+        model.addAttribute("veterinarian", veterinarian);
+        //...
+
+        return "result";
+    }
+
+    @GetMapping("/all")
+    public String findAll(Model model)
+    {
+        model.addAttribute("veterinarians", new ArrayList<VeterinarianModel>());
+
+        //...
+
+        return "all";
     }
 }
--- src/Projects/014-SpringBoot/010-AnimalHospital/AnimalHospitalServiceApp/VeterinarianWebApplication/src/main/java/com/busrapolat/app/web/veterinarian/model/VeterinarianSaveModel.java
+++ src/Projects/014-SpringBoot/010-AnimalHospital/AnimalHospitalServiceApp/VeterinarianWebApplication/src/main/java/com/busrapolat/app/web/veterinarian/model/VeterinarianSaveModel.java
@@ -3,11 +3,70 @@ package com.busrapolat.app.web.veterinarian.model;
 import java.time.LocalDate;
 
 public class VeterinarianSaveModel {
-    public long diplomaNo;
-    public String citizenId;
-    public String firstName;
-    public String middleName;
-    public String lastName;
-    public LocalDate birthDate;
-    public LocalDate registerDate;
+    private long m_diplomaNo;
+    private String m_citizenId;
+    private String m_firstName;
+    private String m_middleName;
+    private String m_lastName;
+    private LocalDate m_birthDate;
+
+    public long getDiplomaNo()
+    {
+        return m_diplomaNo;
+    }
+
+    public void setDiplomaNo(long diplomaNo)
+    {
+        m_diplomaNo = diplomaNo;
+    }
+
+    public String getCitizenId()
+    {
+        return m_citizenId;
+    }
+
+    public void setCitizenId(String citizenId)
+    {
+        m_citizenId = citizenId;
+    }
+
+    public String getFirstName()
+    {
+        return m_firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        m_firstName = firstName;
+    }
+
+    public String getMiddleName()
+    {
+        return m_middleName;
+    }
+
+    public void setMiddleName(String middleName)
+    {
+        m_middleName = middleName;
+    }
+
+    public String getLastName()
+    {
+        return m_lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        m_lastName = lastName;
+    }
+
+    public LocalDate getBirthDate()
+    {
+        return m_birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate)
+    {
+        m_birthDate = birthDate;
+    }
 }
